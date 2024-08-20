d2ad6677c73da409878ca7b0959b341b9256b1ef
bima-taruna <bimataruna21@gmail.com>
2024-08-20 17:30:33 +0700

    create reusable button component

---
+++ src/components/Shared/Button/Button.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+function Button({
+  label = "",
+  type = "button",
+  classname = "",
+  disabled = false,
+  onclick,
+}) {
+  return (
+    <button
+      type={type}
+      className={`btn ${classname}`}
+      disabled={disabled}
+      onClick={onclick}
+    >
+      {label}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  label: PropTypes.string,
+  type: PropTypes.string,
+  classname: PropTypes.string,
+  disabled: PropTypes.bool,
+  onclick: PropTypes.func,
+};
+
+export default Button;
