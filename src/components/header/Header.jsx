8c652b34277b3ed3b4f7d36c00ca2bd29c979679
bima-taruna <bimataruna21@gmail.com>
2024-08-20 12:07:44 +0700

    create basic header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header>
+      <div className="heading">
+        <div className="app-title">Product Roadmap</div>
+      </div>
+      <button>+ Add New Group</button>
+    </header>
+  );
+}
+
+export default Header;
