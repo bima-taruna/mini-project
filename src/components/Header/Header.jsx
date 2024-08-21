da9ff29fdc49f29dca6b869760647d1a29dce764
bima-taruna <bimataruna21@gmail.com>
2024-08-21 11:53:02 +0700

    feat(header): implement feature to open the modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import Button from "../Shared/Button/Button";
+import Modal from "../Shared/Modal/Modal";
 import "./header.css";
 
 function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toogleModal = () => setIsModalOpen(!isModalOpen);
   return (
     <header>
       <div className="heading">
@@ -9,9 +13,14 @@ function Header() {
         <Button
           label="+ Add New Group"
           classname="primary"
-          onclick={() => {}}
+          onclick={toogleModal}
         />
       </div>
+      {isModalOpen && (
+        <Modal title={"Add New Group"} onClose={toogleModal}>
+          asdasd
+        </Modal>
+      )}
     </header>
   );
 }
