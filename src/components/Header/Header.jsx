5812fc0e32a8c5583ddafe1dd0832020a88b8fe7
bima-taruna <bimataruna21@gmail.com>
2024-08-21 12:27:44 +0700

    refactor(header):delete the button , modal and implement add group task button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,13 @@
-import { useState } from "react";
-import Button from "../Shared/Button/Button";
-import Modal from "../Shared/Modal/Modal";
+import AddGroupTaskButton from "./addGroupTaskButton";
 import "./header.css";
 
 function Header() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const toogleModal = () => setIsModalOpen(!isModalOpen);
   return (
     <header>
       <div className="heading">
         <div className="app-title">Product Roadmap</div>
-        <Button
-          label="+ Add New Group"
-          classname="primary"
-          onclick={toogleModal}
-        />
+        <AddGroupTaskButton />
       </div>
-      {isModalOpen && (
-        <Modal title={"Add New Group"} onClose={toogleModal}>
-          asdasd
-        </Modal>
-      )}
     </header>
   );
 }
