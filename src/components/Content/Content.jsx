f024c0d249b3a5aa104ccbd86d487e245a5281be
bima-taruna <bimataruna21@gmail.com>
2024-08-22 14:51:57 +0700

    map the group data

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,6 +1,25 @@
+import GroupContainer from "../Shared/Group Container/GroupContainer";
+import getColor from "../../utils/getColor";
 import "./content.css";
-function Content() {
-  return <div className="content">asdasd</div>;
+function Content({ groupData, loading, error }) {
+  return (
+    <div className="content">
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {groupData &&
+        groupData.map((item, index) => (
+          <div key={item.id}>
+            <GroupContainer
+              title={item.title}
+              description={item.description}
+              classname={getColor(index + 1)}
+            >
+              asdasdas
+            </GroupContainer>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default Content;
