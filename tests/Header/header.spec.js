9903ff5ef63fe4bcf283e80fe98cb124901f412e
bima-taruna <bimataruna21@gmail.com>
2024-08-22 09:14:57 +0700

    create test for add group form

--- tests/Header/header.spec.js
+++ tests/Header/header.spec.js
@@ -22,11 +22,31 @@ test.describe("Header", () => {
   test("clicking button should open modal", async ({ page }) => {
     await page.locator(".primary").click();
     await expect(page.locator(".modal-overlay")).toBeVisible();
-    await page
-      .locator(".modal-close", {
-        name: "X",
-      })
-      .click();
-    await expect(page.locator(".modal-overlay")).not.toBeVisible();
+    // await page
+    //   .locator(".modal-close", {
+    //     name: "X",
+    //   })
+    //   .click();
+    // await expect(page.locator(".modal-overlay")).not.toBeVisible();
+  });
+
+  test("should return an object on form submission", async ({ page }) => {
+    await page.locator(".primary").click();
+    await expect(page.locator(".modal-overlay")).toBeVisible();
+    await page.fill("#group-title", "Group Task 1");
+    await page.fill("#group-desc", "January - March");
+
+    await page.route(
+      `process.env.VITE_API_URL/${process.env.VITE_API_URL}`,
+      (route) => {
+        const request = route.request();
+        const requestBody = request.postDataJSON();
+
+        expect(requestBody).toEqual({
+          title: "Group Task 1",
+          description: "January - March",
+        });
+      }
+    );
   });
 });
