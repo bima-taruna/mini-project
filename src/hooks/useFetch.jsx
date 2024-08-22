75e787e42642f8c1953deee0f4a3bcbe0141a8ac
bima-taruna <bimataruna21@gmail.com>
2024-08-22 14:50:11 +0700

    refactor(usefetch):return set group data

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,44 +2,49 @@ import { useEffect, useState } from "react";
 
 const cache = {};
 
-function useFetch(url, options = {}) {
-  const [data, setData] = useState(cache[url] || null);
-  const [loading, setLoading] = useState(!cache[url]);
+function useFetch(apiUrl) {
+  const [groupData, setGroupData] = useState(cache[apiUrl] || null);
+  const [loading, setLoading] = useState(!cache[apiUrl]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (!url) return;
-
     const fetchData = async () => {
-      if (cache[url]) {
-        setData(cache[url]);
+      if (cache[apiUrl]) {
+        setGroupData(cache[apiUrl]);
         setLoading(false);
         return;
       }
 
       setLoading(true);
       try {
-        const response = await fetch(url, options);
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${import.meta.env.VITE_TOKEN}`,
+          },
+          redirect: "follow",
+        });
         if (!response.ok) {
           throw new Error("There is something wrong with your network");
         }
         const result = await response.json();
-        cache[url] = result;
-        setData(result);
+        cache[apiUrl] = result;
+        setGroupData(result);
       } catch (error) {
         setError(error);
       } finally {
         setLoading(false);
       }
     };
-
-    fetchData();
-  }, [url, options]);
+    fetchData(apiUrl);
+  }, [apiUrl]);
 
   return {
-    data,
+    groupData,
     loading,
     error,
+    setGroupData,
   };
 }
 
