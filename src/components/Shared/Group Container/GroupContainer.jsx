63c4bd77923c4094f0c5044bfc8d6969b08cb3fe
bima-taruna <bimataruna21@gmail.com>
2024-08-22 16:37:24 +0700

    add classname as props to label

--- src/components/Shared/Group Container/GroupContainer.jsx
+++ src/components/Shared/Group Container/GroupContainer.jsx
@@ -1,10 +1,10 @@
 import Label from "../Label/Label";
 import { GoPlusCircle } from "react-icons/go";
-
+import "./group-container.css";
 function GroupContainer({ title, description, classname, children }) {
   return (
     <div className={`group-container ${classname}`}>
-      <Label name={title} />
+      <Label name={title} className={classname} />
       <div className="group-container-desc">{description}</div>
       {children}
       <button className="add-task-btn">
